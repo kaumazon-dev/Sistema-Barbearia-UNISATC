3ccf8fa34f109c43dfcb5520fa975b61e61f9f28
Kauã <kaugame1804@gmail.com>
2026-09-24 22:47:43 -0300

    feature/exceptions errohandler (#11)
    * feat:Feito-criação-do-exeptions.py
    
    * feat:criando-errorHandler

--- backend/app/core/health.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask import jsonify
-
-blueprint_health = Blueprint("health", __name__, url_prefix="/health")
-
-@blueprint_health.route("/", methods=["GET"])
-def verificar_saude():
-    return jsonify({"status": "ok"})
